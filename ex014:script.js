8120cc4b1a2fb5b1bcd87d4261331cb01a9af625
Renan Rocha Azevedo <renanrochaa2007@gmail.com>
2026-07-20 19:18:02 -0300

    Create ex014:script.js

---
+++ ex014:script.js
@@ -0,0 +1,19 @@
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = window.prompt("Digite a hora atual: ")
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+    // BOM DIA!
+    img.src = "fotomanha.jpg"
+    document.body.style.background = "#e2cd9f"
+} else if (hora >= 12 && hora < 18) {
+    img.src = "fototarde.jpg"
+    document.body.style.background = "#f1c27d"
+} else {
+    // BOA NOITE!
+    img.src = "fotonoite.jpg"
+    document.body.style.background = "#51518c"
+}
+}
