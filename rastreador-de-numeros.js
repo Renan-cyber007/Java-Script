dc0103b6adbbf6fddf71f3672f2931fe173f475f
Renan Rocha Azevedo <renanrochaa2007@gmail.com>
2026-09-11 13:19:35 -0300

    Create rastreador-de-numeros.js

---
+++ rastreador-de-numeros.js
@@ -0,0 +1,7 @@
+let num = [8, 2, 5, 1, 4]
+num.push(9)
+num.sort()
+num.length
+console.log(num)
+console.log("O tamanho total do array é " + num.length)
+console.log("O Número 5 ficou na posição " + num.indexOf(5))
